073794232e739f3bc8ec93ef4ec4fd806a9e83fc
Tikoring <swawas930@gmail.com>
2021-11-02 00:20:50 +0900

    HP추가, 피격판정(상대플레이어 대상인식), Attack.cs를 수정해 포탄이 있는 동안 공격을 못하게 제어

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,7 +7,8 @@ public class Projectile : MonoBehaviour
     [SerializeField]
     private Rigidbody2D rd;
     public float power;
-
+    private static bool firePermission = true;
+    public bool FirePermission => firePermission;   //포탄이 발사된 동안 공격을 막기 위함
     void Start()
     {
         print(this);
@@ -15,13 +16,16 @@ public class Projectile : MonoBehaviour
         // print(power);
         rd.velocity = transform.right * power;
         Destroy(gameObject, 15);
+        firePermission = false;
     }
 
-    public void Fire(float _power, GameObject attackPos) {
+    public void Fire (float _power, GameObject attackPos) {
         Projectile instance = this;
         // print("Fire");
         instance.power = _power;
-        Instantiate(instance, attackPos.transform.position, attackPos.transform.rotation);
+        Vector3 pos = attackPos.transform.rotation * Vector3.right;
+        //firePermission = false;
+        Instantiate(instance, attackPos.transform.position + pos, attackPos.transform.rotation);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -29,5 +33,13 @@ public class Projectile : MonoBehaviour
         if (collision.tag == "Stage") {
             Destroy(gameObject);
         }
+        if (collision.tag == "OtherPlayer") {  //상대방을 인식
+            collision.GetComponent<PlayerHP> ().TakeDamage (10);
+            Destroy (gameObject);
+        }
+    }
+
+    private void OnDestroy() {
+        firePermission = true;
     }
 }
