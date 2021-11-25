e4565dbe0312168a8864a639e51764a557aa2aa5
DubuKim <65989844+DubuKim@users.noreply.github.com>
2021-11-25 16:23:33 +0900

    fix Menu UI

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -5,14 +5,37 @@ using UnityEngine;
 public class MenuScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject AudioSlider;
-    public GameObject VolumeText;
+    public GameObject BGMSlider;
+    public GameObject BGMText;
+    public GameObject BGMVolumeText;
+    public GameObject SFXSlider;
+    public GameObject SFXText;
+    public GameObject SFXVolumeText;
+    public GameObject popupPanel;
+    public GameObject quickButton;
     public void showMenu()
     {
-        bool isActive = AudioSlider.activeSelf;
+        BGMSlider.SetActive(true);
+        BGMText.SetActive(true);
+        BGMVolumeText.SetActive(true);
+        SFXSlider.SetActive(true);
+        SFXText.SetActive(true);
+        SFXVolumeText.SetActive(true);
+        popupPanel.SetActive(true);
+        quickButton.SetActive(true);
 
-        AudioSlider.SetActive(!isActive);
-        VolumeText.SetActive(!isActive);
+    }
+
+    public void quickMenu()
+    {
+        BGMSlider.SetActive(false);
+        BGMText.SetActive(false);
+        BGMVolumeText.SetActive(false);
+        SFXSlider.SetActive(false);
+        SFXText.SetActive(false);
+        SFXVolumeText.SetActive(false);
+        popupPanel.SetActive(false);
+        quickButton.SetActive(false);
     }
 
 }
